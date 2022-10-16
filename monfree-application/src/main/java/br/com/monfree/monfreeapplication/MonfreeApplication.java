9e0adf521b1effc622cf2c12e1cee9fc02336472
Igor <igoramarals96@gmail.com>
2022-10-15 22:01:49 -0300

    Initializing project
    Project template created.

---
+++ monfree-application/src/main/java/br/com/monfree/monfreeapplication/MonfreeApplication.java
@@ -0,0 +1,13 @@
+package br.com.monfree.monfreeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonfreeApplication.class, args);
+	}
+
+}
---
+++ monfree-application/src/test/java/br/com/monfree/monfreeapplication/MonfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.monfree.monfreeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
