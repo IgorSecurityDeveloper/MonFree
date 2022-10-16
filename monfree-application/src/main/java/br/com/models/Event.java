e3f5a49e464e1d5370cd73f750972fce3ed9e81a
Igor <igoramarals96@gmail.com>
2022-10-16 04:53:05 -0300

    commit
    commit

---
+++ monfree-application/src/main/java/br/com/models/Event.java
@@ -0,0 +1,41 @@
+package br.com.models;
+
+public class Event{
+    private String name;
+    private int cpf;
+    private int rg;
+    private int birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
